ca19f99ccdca5a8312cb2bbdd421f4807133926f
davidcoats <david@getsafetycone.com>
2024-05-10 00:07:18 -0700

    Interim changes.

--- source/R5T.R0006/Code/EventHandlers.cs
+++ source/R5T.R0006/Code/EventHandlers.cs
@@ -2,11 +2,17 @@
 
 using Microsoft.AspNetCore.Components;
 
+using R5T.T0142;
+using R5T.T0154;
+
 
 namespace R5T.R0006
 {
+    [Obsolete("See R5T.R0015.EventHandlers")]
     [EventHandler("ontransitionend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
     [EventHandler("onanimationend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+    [RazorComponentMarker] // Also put this type in the Razor components list.
+    [UtilityTypeMarker]
     public static class EventHandlers
     {
     }
